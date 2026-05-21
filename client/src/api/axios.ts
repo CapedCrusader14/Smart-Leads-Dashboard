b901165fd1f13b361aff6ed1fb639b0f3c4bb66d
sid <siddharthapandey65093@gmail.com>
2026-05-21 19:19:57 +0530

    Updated code 2

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
 
   baseURL:
-    "https://smart-leads-dashboard-api-ywtj.onrender.com"
+    "https://smart-leads-dashboard-api-ywtj.onrender.com/api"
 
 });
 
