fa2ceed69c5af319c6151d9e5d2a6510bdd8de6d
sid <siddharthapandey65093@gmail.com>
2026-05-21 19:14:59 +0530

    code update

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,93 @@
 import express from "express";
+
 import cors from "cors";
 
 import authRoutes from "./routes/authRoutes";
+
 import leadRoutes from "./routes/leadRoutes";
 
 const app = express();
 
-app.use(cors());
+
+
+
+// MIDDLEWARE
+
+app.use(
+
+  cors({
+
+    origin: "*",
+
+    methods: [
+
+      "GET",
+
+      "POST",
+
+      "PUT",
+
+      "DELETE"
+
+    ],
+
+    credentials: true
+
+  })
+
+);
+
+
+
 
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
 
-app.use("/api/leads", leadRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API Running...");
-});
+
+// ROUTES
+
+app.use(
+
+  "/api/auth",
+
+  authRoutes
+
+);
+
+
+
+
+app.use(
+
+  "/api/leads",
+
+  leadRoutes
+
+);
+
+
+
+
+// TEST ROUTE
+
+app.get(
+
+  "/",
+
+  (req, res) => {
+
+    res.send(
+
+      "API Running..."
+
+    );
+
+  }
+
+);
+
+
+
 
 export default app;
