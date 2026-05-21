4eadc8d77ee652ddb5adafd44d3c6f39232d11a5
sid <siddharthapandey65093@gmail.com>
2026-05-21 19:18:46 +0530

    Updated production backend URL

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
 
   baseURL:
-    "https://YOUR_RENDER_BACKEND.onrender.com/api"
+    "https://smart-leads-dashboard-api-ywtj.onrender.com"
 
 });
 
